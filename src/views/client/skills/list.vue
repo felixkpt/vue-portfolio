1f10ddb2b37de96a6348b7af685a1f2a4ebc227d
kiptoo <felixkpt@gmail.com>
2023-05-29 20:25:00 +0300

    skills sort responsiveness

--- src/views/client/skills/list.vue
+++ src/views/client/skills/list.vue
@@ -6,20 +6,22 @@
           <h3 class="uppercase mb-1">Skills</h3>
         </el-col>
         <el-col>
-          <el-row :gutters="12" v-if="list.length > 0">
-            <div class="card" v-for="skills_category in list" :key="skills_category._id">
-              <h4 class="flex wrap align-center gap-1 mb-1">
-                <div class="before"><img :src="skills_category.featured_image" alt=""></div>
-                <div>{{ skills_category.name }}</div>
-              </h4>
-              <div class="list flex wrap align-center gap-1 mb-1">
-                <ul>
-                  <li v-for="skill in skills_category.skills" :key="skill._id">
-                    <i class="el-icon-circle-check" /><span>{{ skill.name }}</span>
-                  </li>
-                </ul>
+          <el-row :gutters="12" v-if="list.length > 0" justify="center" type="flex">
+            <el-col :span="24" :md="12" v-for="skills_category in list" :key="skills_category._id">
+              <div class="card">
+                <h4 class="flex wrap align-center gap-1 mb-1">
+                  <div class="before"><img :src="skills_category.featured_image" alt=""></div>
+                  <div>{{ skills_category.name }}</div>
+                </h4>
+                <div class="list flex wrap align-center gap-1 mb-1">
+                  <ul>
+                    <li v-for="skill in skills_category.skills" :key="skill._id">
+                      <i class="el-icon-circle-check" /><span>{{ skill.name }}</span>
+                    </li>
+                  </ul>
+                </div>
               </div>
-            </div>
+            </el-col>
           </el-row>
           <el-row :gutters="22" v-else>
             <content-placeholders v-for="indx in [1, 2]" :key="indx" :animated="true" :rounded="true">
