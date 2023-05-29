47ac9b64a1d6c6358b9ad2ec82ac3025e8518129
kiptoo <felixkpt@gmail.com>
2023-05-29 19:24:55 +0300

    project view cleanup

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:felixkpt/vue-portfolio.git master:gh-pages
+git push -f git@github.com:felixkpt/vue-portfolio.git master:vue-portfolio-master
 
 cd -
